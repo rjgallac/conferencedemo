f960fa2ef1c859d2f5a9ece53f5c4a50d67779ae
Robert Gallacher <rjgallac@hotmail.com>
2020-11-05 21:41:32 +0000

    update

--- src/main/java/com/democonference/democonference/controllers/SessionsController.java
+++ src/main/java/com/democonference/democonference/controllers/SessionsController.java
@@ -2,10 +2,9 @@ package com.democonference.democonference.controllers;
 
 import com.democonference.democonference.models.Session;
 import com.democonference.democonference.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +15,30 @@ public class SessionsController {
     private SessionRepository sessionRepository;
 
     @GetMapping
-    public List<Session> list(){
+    public List<Session> list() {
         return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
